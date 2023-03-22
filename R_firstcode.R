e41e38fe0c7add789b1ec5d6c620268f025bd663
giuliapanelli2000 <127427348+giuliapanelli2000@users.noreply.github.com>
2023-03-22 17:40:16 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -3,3 +3,25 @@
 install.packages ("raster")
 # per controllare se si sono installati <- library 
 library (raster)
+#install.packages( "raster" )
+library(raster)
+#importa i dati, impostando la directory di lavoro
+#setwd ("C:/lab/")#windows
+
+library(raster)
+setwd ("C:/Telerilevamento_lab")
+getwd ()
+
+l2011 <- brick("p224r63_2011_masked.grd")
+plot (l2011)
+cl <- colorRampPalette (c("red", "orange", "yellow")) (100) 
+# per cambiare i colori, abbiamo tre colori di base e le diverse sfumature
+# abbinare il cambio di colori alle carte, assieme alle 100 sfumature diverse
+plot (l2011)
+
+#tracciare un elemento
+plot (l2011 [[4]], col= cl )
+plot (l2011 $ b4_sre, col=cl) #il dollaro lega dei pezzi fra loro
+nir <- l2011 [[4]] #oppure nir <- l2011$B4_sre
+plot (nir, col = cl)
+
