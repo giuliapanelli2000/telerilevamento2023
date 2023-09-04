48d09d094c93ce7bed1d0b5b356ede51afa17102
giuliapanelli2000 <127427348+giuliapanelli2000@users.noreply.github.com>
2023-09-04 18:22:54 +0200

    Update R_code_vegetation_indices.R

--- R_code_vegetation_indices.R
+++ R_code_vegetation_indices.R
@@ -25,7 +25,7 @@ plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
 # l'acqua in infrarosso assorbe la luce? 
 # calcolo indice di vegetazione
 
-#esercizio: calcola DVI per 1992
+#esercizio: calcola DVI per 1992. DVI: different vegetation index
 # band infrarosso meno la banda del rosso
 # il nir è l'infrarosso 
 
@@ -51,3 +51,36 @@ dvil2006
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 
 plot (dvil2006, col=cl)
+
+par(mfrow=c(2,1))     # carico le due immagini ottenute in precedenza  in una singola immagine a 2 righe e una colonna 
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin") 
+#per metterle invece una accanto all'altra:
+par(mfrow=c(1,2))     
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin") 
+
+library(raster)
+setwd("C:/Telerilevamento_lab")
+getwd()
+#analisi multitemporale, differenza tra due DVI (1992 vs 2006),se la differenza è alta indica maggiore distruzione in questo intervallo di tempo
+difdvi = dvil1992 - dvil2006
+cld <- colorRampPalette(c('blue','white','red')) (100)
+plot(difdvi, col=cld) 
+#Non si possono paragonare due immagini che hanno bit diversi, per sistemare il problema si normailzza il dvi -> NDVI (cioè normal dvi)
+# NDVI 1992 normalizziamo sulla somma delle due bande (differenza diviso la somma), il numero di bit di una immagine è la risoluzione radiometrica,spettrale  e spaziale
+ndvi1992 = dvil1992 / (l1992[[1]] + l1992[[2]])
+plot(ndvi1992, col=cl)  
+#calcoliamo il NDVI del 2006
+ndvi2006 = dvil2006 / (l2006[[1]] + l2006[[2]])
+plot(ndvi2006, col=cl)
+
+#differenza tra i due ndvi
+difndvi = ndvi1992 - ndvi2006
+plot(difndvi, col=cld) 
+
+# Multiframe with NDVI1992 on top of the NDVI2006 image
+par(mfrow=c(2,1))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
